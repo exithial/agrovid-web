31888204b0e94ebfde2c7035f26236baeb16dfa6
Enrique Solis <enriquesolisgalvez@gmail.com>
2024-07-10 22:05:50 -0400

    🎉 Se inicializa proyecto con react js firebase login

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6g_TyFdFB8zvUMU5r5jg-fVeZC6dmM_c",
+  authDomain: "agrovid-fc435.firebaseapp.com",
+  projectId: "agrovid-fc435",
+  storageBucket: "agrovid-fc435.appspot.com",
+  messagingSenderId: "149705864372",
+  appId: "1:149705864372:web:f32584521c5261a02853e1",
+  measurementId: "G-0MG5FXHWQX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
