0e5068e5e68c6c84f5ab0d81ded0ca8a50b31298
Enrique Solis <enriquesolisgalvez@gmail.com>
2024-07-12 14:45:18 -0400

    🚧 Se obtiene información sobre el portafolio de los jardineros y se agrega loading mientras eso sucede

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,60 @@
-import React from "react";
-import { Button, Container, Navbar } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Modal,
+  Navbar,
+  Row,
+  Spinner,
+} from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
+import { db } from "../firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { objectIsEmpty } from "../utils";
 
 const Home = () => {
   const { logOut, user } = useUserAuth();
   const navigate = useNavigate();
-  const handleLogout = async () => {
+  const [resume, setResume] = useState(null);
+  const [loadingResume, setLoadingResume] = useState(true);
+
+  async function handleLogout() {
     try {
       await logOut();
       navigate("/");
     } catch (error) {
       console.log(error.message);
     }
-  };
+  }
+
+  async function getResume() {
+    const resumeRef = collection(db, "resume");
+    const q = query(resumeRef, where("email", "==", user.email));
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      console.log("No matching documents.");
+    } else {
+      querySnapshot.forEach((doc) => {
+        setResume(doc.data());
+      });
+    }
+    setLoadingResume(false);
+  }
+
+  useEffect(() => {
+    if (!objectIsEmpty(user)) {
+      getResume();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
   return (
     <Container style={{ minHeight: "100vh", minWidth: "100vw" }}>
       <Navbar className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">
-            Bienvenido {user && user.email}
-          </Navbar.Brand>
+          <Navbar.Brand href="#home">Bienvenido {user?.email}</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
@@ -31,9 +65,19 @@ const Home = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="p-4 box mt-3 text-center">
-        
-      </div>
+      <div className="p-4 box mt-3 text-center"></div>
+      <Modal show={loadingResume} size="sm" centered>
+        <Modal.Body>
+          <Row
+            style={{ height: 200 }}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <Col md="auto">
+              <Spinner animation="border" />
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 };
